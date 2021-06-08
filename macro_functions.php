1afb4a4943ae409d627a10320b1dc58f88c41235
Parad0xe <parad0xe@protonmail.com>
2021-06-08 18:21:30 +0200

    Fix macro functions

--- macro_functions.php
+++ macro_functions.php
@@ -4,7 +4,9 @@ if(!function_exists("endsWith")) {
     function endsWith($in, $search) {
         return substr($in, -strlen($search)) === $search;
     }
+}
 
+if(!function_exists("startsWith")) {
     function startsWith($in, $search) {
         return substr($in, 0, strlen($search)) === $search;
     }
